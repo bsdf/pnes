8c41fd28dd670ba70d62a9afce87025548a98d83
BEN ENGLISCH <EMAILBEN145@gmail.com>
2011-09-24 18:52:37 -0500

    removed build dir

--- build/PNES.build/Debug/Build & Install.build/Script-82D814E40F1D81BE00EF8CF5.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mkdir -p ~/Library/Application\ Support/OpenEmu/Cores
-rm -rf ~/Library/Application\ Support/OpenEmu/Cores/$PROJECT_NAME.oecoreplugin
-cp -rf "$BUILT_PRODUCTS_DIR/$PROJECT_NAME.oecoreplugin"{,.dSYM} ~/Library/Application\ Support/OpenEmu/Cores/
-
